9f72376afc256c70fe941bef8cfc5443b4c5036c
Bethany Marie Ward <bethany.m.ward@students.plymouth.ac.uk>
2020-04-23 12:15:38 +0100

    Improved comments in bikeshare.r

--- bikeshare.r
+++ bikeshare.r
@@ -10,10 +10,10 @@ head(wash)
 head(chi, 8)
 
 library(ggplot2)
-library(grDevices) # I'm using this because I find it way easier for working with colours
+library(grDevices)
 
 
-# Base plot overlayed by two histograms to portray the subcriber birth years of the two cities
+# Member birth years of the two cities
 ggplot()  + 
     geom_histogram(
         aes(x = Birth.Year, y = ..density.., fill = "New York City"),           
@@ -31,7 +31,7 @@ ggplot()  +
     scale_fill_manual("", values = c(adjustcolor('tomato', alpha = 0.75), adjustcolor('violet', alpha = 0.75)), breaks = c("New York City", "Chicago")) + 
     scale_color_discrete(guide = FALSE)
 
-# Function writes a title for any summary in this project
+# Writes a title for any summary in this project
 mySummaryTitle <- function(x = "x", bySummary = FALSE, y = "y", city = "Washington DC") {
     if (bySummary == FALSE) {
         writeLines(paste(x, "of", y, "in", city))
@@ -40,16 +40,16 @@ mySummaryTitle <- function(x = "x", bySummary = FALSE, y = "y", city = "Washingt
     }
 }
 
-# Summarise birth year data
+# Birth year data
 mySummaryTitle("Birth Years", FALSE, "Bikeshare Subscribers", "New York City")
 summary(ny$Birth.Year, subset(ny, !is.na(Birth.Year))) 
 mySummaryTitle('Birth Years', FALSE, "Subscribers", "Chicago City")
 summary(chi$Birth.Year, subset(chi, !is.na(Birth.Year)))
 
-# Add a column for age for concise code
+# Add a column Age to NYC data
 ny$Age <- (as.numeric(format(as.Date(ny$Start.Time), '%Y')) - ny$Birth.Year)
 
-# Base plot overlayed by scatterplot displaying age:trip_duration
+# Age:trip_duration
 ggplot(aes(x = Age, y = Trip.Duration), data = subset(ny, !is.na(Age))) +
     geom_jitter(alpha = 1/100, lwd = 2) +
     scale_y_continuous(limits = c(0, 5000)) +
@@ -57,15 +57,15 @@ ggplot(aes(x = Age, y = Trip.Duration), data = subset(ny, !is.na(Age))) +
     labs(x = 'Age', y = 'Trip Duration (Seconds)') +
     ggtitle('Bikeshare Ride Durations by Subscriber Age in New York City') 
 
-# summarised trip duration data to display the extremity of outliers 
+# Trip duration data displays the extremity of outliers 
 mySummaryTitle('Duration', FALSE, 'Bikeshare Trips', 'New York City')
 summary(ny$Trip.Duration)
 
-# summarised age data
+# Age data
 mySummaryTitle('Age', FALSE, 'Bikeshare Subscribers', 'New York City')
 summary(ny$Age)
 
-# adjusted boxplot to take care of major outliers
+# Gender:trip_duration (adjusted to ignore extremities)
 ggplot(aes(x = Gender, y = Trip.Duration), data = subset(chi, Gender != '')) +
     geom_boxplot() +
     coord_cartesian(ylim = c(0, 3000)) +
@@ -73,7 +73,7 @@ ggplot(aes(x = Gender, y = Trip.Duration), data = subset(chi, Gender != '')) +
     labs(x = 'Gender', y = 'Trip Duration (Seconds)') +
     ggtitle('Bikeshare Bike Ride Duration by Gender in Chicago City')
 
-# summarise trip durations by gender
+# Trip durations by gender in Chicago
 mySummaryTitle('Bikeshare Trip Durations', TRUE, 'Gender', 'Chicago City')
 by(chi$Trip.Duration, chi$Gender, summary)
 
